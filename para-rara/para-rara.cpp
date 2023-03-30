a63ba16ebab46360f1aa769df5bf35aa88379e1c
ulinad <danilu2537@gmail.com>
2023-03-30 19:10:00 +0300

    push

--- para-rara/para-rara.cpp
+++ para-rara/para-rara.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
-int main() {
-	const int size = 2;
-	int arr1[size][size] = { {1, 0}, 
-							 {3, 2} };
-	int arr2[size][size] = { {2, 1},
-							 {0, 1} };
-	int arr3[size][size];
+int add(int a, int b) {
+	int c = a + b;
+	return c;
+}
 
+int main() {
+	int d = 3;
+	int e = 4;
+	int f = add(d, e);
+	cout << f << endl;
 }
