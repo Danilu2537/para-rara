64732c534abea11115ff28141b85cfccf13e031d
ulinad <danilu2537@gmail.com>
2023-03-30 18:42:30 +0300

    BAN

--- para-rara/para-rara.cpp
+++ para-rara/para-rara.cpp
@@ -3,30 +3,11 @@
 using namespace std;
 
 int main() {
-	const int size = 5;
-	int arr[size][size];
-	for (int i = 0; i < size; i++)
-		for (int j = 0; j < size; j++)
-			arr[i][j] = rand() % 10;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++)
-			cout << arr[i][j] << " ";
-		cout << endl;
-	}
-	cout << endl;
-	int arr1[size][size];
-	for (int i = 0, k = size-1; i < size; i++, k--)
-	{
-		for (int j = 0; j < size; j++)
-		{
-			arr1[i][j] = arr[j][k];
-		}
-	}
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++)
-			cout << arr1[i][j] << " ";
-		cout << endl;
-	}
-	cout << endl;
+	const int size = 2;
+	int arr1[size][size] = { {1, 0}, 
+							 {3, 2} };
+	int arr2[size][size] = { {2, 1},
+							 {0, 1} };
+	int arr3[size][size];
 
 }
